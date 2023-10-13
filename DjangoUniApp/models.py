ad0e4f59d522b336ffba7447fefd1e51f697ad4d
christian burns <christianburns03@gmail.com>
2023-10-13 09:40:02 -0300

    primer commit

---
+++ DjangoUniApp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Student(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellidos = models.CharField(max_length=100)
+    edad = models.IntegerField()
+    email = models.EmailField()
+
